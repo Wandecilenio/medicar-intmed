5fc641bbb8145b78758667148c9a885a9ad25bba
Wandecilenio01 <wandecileniomartins@gmail.com>
2020-11-17 12:39:21 -0300

    Adiciona possibilidade de filtro em especialidades

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .serializers import EspecialidadeSerializer
 from administration.models import Especialidade
 
 class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = EspecialidadeSerializer
     queryset = Especialidade.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
